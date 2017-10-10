1d7a4ac9f2eb4e58f95271f196da68b861995022
enriplaso <enriplaso@gmail.com>
2017-10-10 14:28:56 +0200

    Search fileterng

--- src/components/List-page.vue
+++ src/components/List-page.vue
@@ -1,25 +1,24 @@
 <template>
   <!-- you define your Vue template here -->
-  <div>
+  <div class="item-list-container">
+    <q-search v-model="search" />
     <q-list highlight>
       <q-list-header>Fridge items</q-list-header>
-      <q-item>
+      <q-item v-for="fridgeItem in filteredList" v-bind:key="fridgeItem.name"  v-on:click="openListItemDetails()">
         <q-item-main>
-          <q-item-tile label>John Doe</q-item-tile>
+          <q-item-tile label>{{fridgeItem.name}}</q-item-tile>
           <q-item-tile sublabel>Quasar enthusiast</q-item-tile>
         </q-item-main>
         <q-item-side right>
           <q-item-tile icon="chat_bubble" color="green" />
         </q-item-side>
       </q-item>
+      <q-item-separator inset />
     </q-list>
   </div>
 </template>
 <script>
 import {
-  dom,
-  event,
-  openURL,
   QLayout,
   QToolbar,
   QToolbarTitle,
@@ -29,14 +28,9 @@ import {
   QListHeader,
   QItem,
   QItemSide,
-  QItemMain
+  QItemMain,
+  QSearch
 } from 'quasar'
-
-
-// This is where your Javascript goes
-// to define your Vue component, which
-// can be a Layout, a Page or your own
-// component used throughout the app.
 export default {
   components: {
     QLayout,
@@ -48,10 +42,38 @@ export default {
     QListHeader,
     QItem,
     QItemSide,
-    QItemMain
+    QItemMain,
+    QSearch
+  },
+  data () {
+    return {
+      search: '',
+      fridgeItemsList: [
+        {name: 'bannana', quantity: 3, expiredDate: '28/10/2017', comment: 'xxxx'},
+        {name: 'yogurt', quantity: 3, expiredDate: '28/10/2017', comment: 'xxxx'},
+        {name: 'Cheese', quantity: 3, expiredDate: '28/10/2017', comment: 'xxxx'},
+        {name: 'Milk', quantity: 3, expireddate: '28/10/2017', comment: 'xxxx'}
+      ]
+    }
+  },
+  computed: {
+    filteredList () {
+      return this.fridgeItemsList.filter(item => {
+        return item.name.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  },
+  methods: {
+    openListItemDetails () {
+      alert('TEST')
+    }
   }
 }
 </script>
-<style>
+<style lang="stylus">
 /* This is where your CSS goes */
+.item-list-container
+  position absolute
+  height 100%
+  width 100%
 </style>
